c88c2b2e37cde60d8bea8c76428bafed488c71b2
rwang067 <wangrui1994@126.com>
2019-05-21 10:35:07 +0800

    update progress info

--- progress_of_graphwalker.cpp
+++ progress_of_graphwalker.cpp
@@ -16,3 +16,14 @@
     terminate called after throwing an instance of 'std::bad_alloc'
     what():  std::bad_alloc
     Aborted (core dumped)
+
+//modified in 20190521
+TBD:
+    1>. turn on PageCache, and set nmblocks = 1
+    2>. turn off PageCache with nocache command, and set nmblocks as large as possible.
+    the latter is a little bit of improvement upon the former.
+    the exp results consult to : my work/exp/GraphWalker2.0/graphwalker.xlsx -- nmblocks--Crawl
+
+Attention : 
+    1>. With PageCache turning on, effects exist between differnt experiments.
+    2>. open very large file cost a lot.(around 4s for a 500GB file)
