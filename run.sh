0c8d471fee07a0163d8e8a8379b18d42b619dc71
rwang067 <wangrui1994@126.com>
2019-06-20 18:07:20 +0800

    runsh

--- run.sh
+++ run.sh
@@ -1,80 +1,40 @@
-echo "2019.6.14 " >> graphwalker_metrics.txt.statistics
-echo "observe the impact of nmblocks (clear pagecache each time) in 64GB R730, app = msppr, dataset = Twitter" >> graphwalker_metrics.txt.statistics
-
-### 64GB R730, SSD, Twitter
-################################################################################################
-for(( numsources = 1000; numsources <= 1000; numsources*=10))
-do
-    echo "numsources = " $numsources >> graphwalker_metrics.txt.statistics
-    echo "Turn on Page Cache, nmblocks = 1" >> graphwalker_metrics.txt.statistics
-    for(( times = 0; times < 3; times++))
-    do
-        echo "times = " $times " from echo"
-        sync; echo 1 > /proc/sys/vm/drop_caches
-        ./bin/apps/msppr file ../../raid0_mnop/Twitter/twitter_rv.net firstsource 12 numsources $numsources nmblocks 1
-    done
-    for(( nmblocks = 20; nmblocks <= 200; nmblocks+=20))
-    do
-        echo "Turn on Page Cache, nmblocks = " $nmblocks >> graphwalker_metrics.txt.statistics
-        for(( times = 0; times < 3; times++))
-        do
-            echo "times = " $times " from echo"
-            sync; echo 1 > /proc/sys/vm/drop_caches
-            ./bin/apps/msppr file ../../raid0_mnop/Twitter/twitter_rv.net firstsource 12 numsources $numsources nmblocks $nmblocks
-        done
-    done
-done
-
-echo "2019.6.14 " >> graphwalker_metrics.txt.statistics
-echo "observe the impact of nmblocks (clear pagecache each time) in 64GB R730, app = msppr, dataset = Yahoo" >> graphwalker_metrics.txt.statistics
-
-### 64GB R730, SSD, Yahoo
-################################################################################################
-for(( numsources = 1000; numsources <= 1000; numsources*=10))
-do
-    echo "numsources = " $numsources >> graphwalker_metrics.txt.statistics
-    echo "Turn on Page Cache, nmblocks = 1" >> graphwalker_metrics.txt.statistics
-    for(( times = 0; times < 3; times++))
-    do
-        echo "times = " $times " from echo"
-        sync; echo 1 > /proc/sys/vm/drop_caches
-        ./bin/apps/msppr file ../../raid0_mnop/Yahoo/yahoo-webmap.txt firstsource 4 numsources $numsources nmblocks 1
-    done
-    for(( nmblocks = 100; nmblocks <= 900; nmblocks+=100))
-    do
-        echo "Turn on Page Cache, nmblocks = " $nmblocks >> graphwalker_metrics.txt.statistics
-        for(( times = 0; times < 3; times++))
-        do
-            echo "times = " $times " from echo"
-            sync; echo 1 > /proc/sys/vm/drop_caches
-            ./bin/apps/msppr file ../../raid0_mnop/Yahoo/yahoo-webmap.txt firstsource 4 numsources $numsources nmblocks $nmblocks
-        done
-    done
-done
-
-echo "2019.6.11 " >> graphwalker_metrics.txt.statistics
-echo "observe the impact of nmblocks (clear pagecache each time) in 64GB R730, app = msppr, dataset = Crawl" >> graphwalker_metrics.txt.statistics
+echo "2019.6.20 " >> graphwalker_metrics.txt.statistics
+echo "observe the impact of block size (clear pagecache each time) in 64GB R730, app = msppr, dataset = Crawl" >> graphwalker_metrics.txt.statistics
 
 ### 64GB R730, SSD, Crawl
 ################################################################################################
-for(( numsources = 1000; numsources <= 1000; numsources*=10))
+for(( numsources = 100; numsources <= 100; numsources*=10))
 do
     echo "numsources = " $numsources >> graphwalker_metrics.txt.statistics
-    echo "Turn on Page Cache, nmblocks = 1" >> graphwalker_metrics.txt.statistics
-    for(( times = 0; times < 3; times++))
+    echo "Turn on Page Cache, clear pagecache each time, nmblocks = 1" >> graphwalker_metrics.txt.statistics
+    for(( blocksize_kb = 4096; blocksize_kb >= 512; blocksize_kb/=2))
     do
-        echo "times = " $times " from echo"
-        sync; echo 1 > /proc/sys/vm/drop_caches
-        ./bin/apps/msppr file ../../raid0_mnop/Crawl/crawl.txt firstsource 0 numsources $numsources nmblocks 1
-    done
-    for(( nmblocks = 200; nmblocks <= 2600; nmblocks+=200))
-    do
-        echo "Turn on Page Cache, nmblocks = " $nmblocks >> graphwalker_metrics.txt.statistics
+        echo "Turn on Page Cache, blocksize_kb = " $blocksize_kb >> graphwalker_metrics.txt.statistics
         for(( times = 0; times < 3; times++))
         do
             echo "times = " $times " from echo"
+            free -m
             sync; echo 1 > /proc/sys/vm/drop_caches
-            ./bin/apps/msppr file ../../raid0_mnop/Crawl/crawl.txt firstsource 0 numsources $numsources nmblocks $nmblocks
+            free -m
+            ./bin/apps/msppr file ../../raid0_mnop/Crawl/crawl.txt firstsource 0 numsources $numsources nmblocks 1 blocksize_kb $blocksize_kb
+        done
+    done
+done
+
+for(( numsources = 10000; numsources <= 10000; numsources*=10))
+do
+    echo "numsources = " $numsources >> graphwalker_metrics.txt.statistics
+    echo "Turn on Page Cache, clear pagecache each time, nmblocks = 1" >> graphwalker_metrics.txt.statistics
+    for(( blocksize_kb = 8192; blocksize_kb >= 1024; blocksize_kb/=2))
+    do
+        echo "Turn on Page Cache, blocksize_kb = " $blocksize_kb >> graphwalker_metrics.txt.statistics
+        for(( times = 0; times < 3; times++))
+        do
+            echo "times = " $times " from echo"
+            free -m
+            sync; echo 1 > /proc/sys/vm/drop_caches
+            free -m
+            ./bin/apps/msppr file ../../raid0_mnop/Crawl/crawl.txt firstsource 0 numsources $numsources nmblocks 1 blocksize_kb $blocksize_kb
         done
     done
 done
