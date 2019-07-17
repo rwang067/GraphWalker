c255f874219632812a5660bcdea31213149599d7
rwang067 <wangrui1994@126.com>
2019-07-17 16:41:31 +0800

    rrw nmblock

--- apps/rawrandomwalks.cpp
+++ apps/rawrandomwalks.cpp
@@ -84,7 +84,7 @@ int main(int argc, const char ** argv){
     hid_t L = get_option_int("L", 4); // Number of steps per walk
     float prob = get_option_float("prob", 0.2); // prob of chose min step
     // unsigned long long blocksize_kb = get_option_long("blocksize_kb", 32768); // Size of block, represented in KB
-    // bid_t nmblocks = get_option_int("nmblocks", 10); // number of in-memory blocks
+    bid_t nmblocks = get_option_int("nmblocks", 0); // number of in-memory blocks
     
     logstream(LOG_DEBUG) << "N R L : " << N << " " << R << " " << L << std::endl;
     // run
@@ -94,7 +94,9 @@ int main(int argc, const char ** argv){
     unsigned long long blocksize_kb = program.compBlockSize(R);
     /* Detect the number of shards or preprocess an input to create them */
     bid_t nblocks = convert_if_notexists(filename, blocksize_kb);
-    bid_t nmblocks = program.compNmblocks(blocksize_kb);
+    if(nmblocks == 0){
+        nmblocks = program.compNmblocks(blocksize_kb);
+    }
     if(nmblocks > nblocks) nmblocks = nblocks;
 
     graphwalker_engine engine(filename, blocksize_kb, nblocks,nmblocks, m);
--- src/api/datatype.hpp
+++ src/api/datatype.hpp
@@ -11,7 +11,7 @@
 #define	EDGE_SIZE	256 * 1024 * 1024 // 256M edges in csr buffer in preprocess
 #define	WALK_BUFFER_SIZE	20480 // most 1024 walks in a in-memory walk buffer
 #define	MEM_BUDGET	56 * 1024 * 1024 // for 64GB memory machine
-// #define	MEM_BUDGET	6 * 1024 * 1024 // for 8GB memory machine
+// #define	MEM_BUDGET	4 * 1024 * 1024 // for 8GB memory machine
 
 typedef uint32_t vid_t;
 typedef uint64_t eid_t;
