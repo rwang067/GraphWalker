aeb63225fcdf3d829f49e92107d6adb23b19994a
rwang067 <wangrui1994@126.com>
2019-09-03 18:23:01 +0800

    blcokwithNext

--- src/engine/graphwalker.hpp
+++ src/engine/graphwalker.hpp
@@ -280,8 +280,7 @@ public:
         while( userprogram.hasFinishedWalk(*walk_manager) ){
             blockcount++;
             m.start_time("1_chooseBlock");
-            // exec_block = walk_manager->chooseBlock(prob);
-            exec_block = walk_manager->blockWithNext(exec_block);
+            exec_block = walk_manager->chooseBlock(prob);
             m.stop_time("1_chooseBlock");
             findSubGraph(exec_block, beg_pos, csr, &nverts, &nedges);
 
